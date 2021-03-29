5b287510951bcb6ead5375df383aca1c353ae9eb
yzlzr <37923345+yzlzr@users.noreply.github.com>
2021-03-29 17:52:47 +0800

    Update diy-2.sh

--- build/openwrt/diy-2.sh
+++ build/openwrt/diy-2.sh
@@ -11,4 +11,7 @@
 #
 #
 # 修改网关（把192.168.2.2改成你需要的就行）
-#sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+#修复核心及添加温度显示
+sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
